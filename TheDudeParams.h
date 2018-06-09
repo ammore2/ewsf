b72d676f0342e801e4882d82d289cc3327d5d7b3
Ammar Annowairah <alnouirah@gmail.com>
2018-06-09 22:57:25 +0300

    Initial commit

---
+++ TheDudeParams.h
@@ -0,0 +1,5 @@
+#define MY_SSID "MIFI-0C9C" //wifi name
+#define MY_PWD "Al@m123" // wifi password
+#define MY_HOST "192.168.1.2" // ex: thanawi.app
+#define MY_HOSTIP "192.168.1.2"// ex: thanawi.app
+#define PAGE_URL "192.168.1.3/ewsf/report.php" //ex: /insert.php
